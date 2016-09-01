1d3a3c6d2fe9e744e7ab6a6b62415c94e8abf060
XProger <xproger@list.ru>
2016-09-01 04:50:02 +0300

    gzip compression

--- src/web/build.bat
+++ src/web/build.bat
@@ -1,2 +1,3 @@
 set SRC=main.cpp
-em++ %SRC% -O2 --llvm-opts 2 --closure 1 -std=c++11 -o OpenLara.js --preload-file ./LEVEL2_DEMO.PHD -I..\
+call em++ %SRC% -O2 --llvm-opts 2 --closure 1 -std=c++11 -o OpenLara.js --preload-file ./LEVEL2_DEMO.PHD -I..\
+gzip.exe -9 -f OpenLara.data OpenLara.js OpenLara.js.mem
