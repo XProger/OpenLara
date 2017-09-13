64136e4748e394fc29eb3bb94313a643ae32f2b1
XProger <xproger@list.ru>
2017-09-13 04:35:35 +0300

    #15 fix web build

--- src/platform/web/build.bat
+++ src/platform/web/build.bat
@@ -3,7 +3,6 @@ cls
 set SRC=main.cpp ../../libs/stb_vorbis/stb_vorbis.c
 set PROJ=OpenLara
 set FLAGS=-O3 -Wno-deprecated-register --llvm-opts 2 -fmax-type-align=2 -std=c++11 -Wall -I../../
-set PRELOAD=./level/TITLE.PSX;./level/TITLEH.PCX
 echo.
-call em++ %SRC% %FLAGS% -o %PROJ%.js --preload-file %PRELOAD%
+call em++ %SRC% %FLAGS% -o %PROJ%.js --preload-file ./level/TITLE.PSX --preload-file ./level/TITLEH.PCX
 gzip.exe -9 -f %PROJ%.data %PROJ%.js %PROJ%.js.mem
