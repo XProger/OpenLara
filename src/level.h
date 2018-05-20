65eaca2f53ddcf92b6c916dda64f94bc2ff46999
XProger <xproger@list.ru>
2018-05-21 02:59:47 +0300

    fix set default RT after prepareBackground call

--- src/level.h
+++ src/level.h
@@ -285,6 +285,8 @@ struct Level : IGame {
             Core::reset();
             Core::beginFrame();
             inventory.prepareBackground();
+            Core::setTarget(NULL, 0);
+            Core::validateRenderState();
             Core::endFrame();
         }
     }
