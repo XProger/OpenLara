20816951b1de299ac05e5fe17c8f4fdf59969166
XProger <xproger@list.ru>
2019-02-15 02:09:18 +0300

    #15 fix RPI build

--- src/gapi_gl.h
+++ src/gapi_gl.h
@@ -59,6 +59,8 @@
     #define GL_COMPARE_REF_TO_TEXTURE   0x884E
 
     #undef  GL_RG
+    #undef  GL_RG32F
+    #undef  GL_RG16F
     #undef  GL_RGBA32F
     #undef  GL_RGBA16F
     #undef  GL_HALF_FLOAT
@@ -66,11 +68,17 @@
     #define GL_RG           GL_RGBA
     #define GL_RGBA32F      GL_RGBA
     #define GL_RGBA16F      GL_RGBA
+    #define GL_RG32F        GL_RGBA
+    #define GL_RG16F        GL_RGBA
     #define GL_HALF_FLOAT   GL_HALF_FLOAT_OES
 
+    #define GL_TEXTURE_3D           0
+    #define GL_TEXTURE_WRAP_R       0
     #define GL_DEPTH_STENCIL        GL_DEPTH_STENCIL_OES
     #define GL_UNSIGNED_INT_24_8    GL_UNSIGNED_INT_24_8_OES
-    
+
+    #define glTexImage3D(...) 0
+
     #define glGenVertexArrays(...)
     #define glDeleteVertexArrays(...)
     #define glBindVertexArray(...)
@@ -1153,7 +1161,13 @@ namespace GAPI {
 
         GLSL_HEADER_VERT[0] = GLSL_HEADER_FRAG[0] = 0;
     #ifdef _GAPI_GLES
-        if (WEBGL_VERSION == 1) {
+        bool GLES3 = false;
+        #ifdef _OS_WEB
+            GLES3 = WEBGL_VERSION != 1;
+        #else
+            GLES3 = false;
+        #endif
+        if (!GLES3) {
             strcat(GLSL_HEADER_VERT, "#define VERTEX\n"
                                      "precision lowp  int;\n"
                                      "precision highp float;\n");
