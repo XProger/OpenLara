938b400cbc24522d3a1592c7c2a4c335279b6e65
vanfanel <redwindwanderer@gmail.com>
2019-02-20 21:04:07 +0100

    Add SDL2 platform

--- src/core.h
+++ src/core.h
@@ -27,6 +27,12 @@
     //#define _GAPI_VULKAN
 
     extern void osToggleVR(bool enable);
+#elif __SDL2__
+    #define _OS_SDL2   1
+    #define _GAPI_GL   1
+    #define _GAPI_GLES 1
+
+    #define DYNGEOM_NO_VBO
 #elif __RPI__
     #define _OS_RPI    1
     #define _GAPI_GL   1
