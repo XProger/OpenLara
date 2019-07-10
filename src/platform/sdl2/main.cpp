e02e582db89dd384fdb47ac96857bb1940476974
Manuel Alfayate Corchete <redwindwanderer@gmail.com>
2019-07-10 06:35:21 +0200

    Fix building for SDL2 in GLES mode, and fix black screen on GLES mode. (#195)

--- src/platform/sdl2/main.cpp
+++ src/platform/sdl2/main.cpp
@@ -439,10 +439,6 @@ int main(int argc, char **argv) {
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE,   8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,  8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
