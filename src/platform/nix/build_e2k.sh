6fe0191cc298a5a78d7cd2614678e1f3c952b401
XProger <xproger@list.ru>
2019-09-26 01:13:24 +0300

    add elbrus build script

---
+++ src/platform/nix/build_e2k.sh
@@ -0,0 +1,3 @@
+set -e
+g++ -std=c++11 -O4 -s -fno-exceptions -fno-rtti -ffunction-sections -fdata-sections -Wl,--gc-sections -Wno-invalid-source-encoding -DELBRUS -DNDEBUG -D_POSIX_THREADS -D_POSIX_READER_WRITER_LOCKS main.cpp ../../libs/stb_vorbis/stb_vorbis.c ../../libs/minimp3/minimp3.cpp ../../libs/tinf/tinflate.c -I../../ -o../../../bin/OpenLara -lX11 -lGL -lm -lpthread -lpulse-simple -lpulse
+strip ../../../bin/OpenLara --strip-all --remove-section=.comment --remove-section=.note
