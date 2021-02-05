6b6559f23a643d518f92544adbde1dad9439dac0
iZarif <aydar.js@gmail.com>
2021-02-05 17:02:39 +0300

    add premake5 build script for unix systems

---
+++ src/platform/nix/premake5.lua
@@ -0,0 +1,24 @@
+workspace "OpenLara"
+   configurations { "Debug", "Release" }
+
+project "OpenLara"
+   kind "ConsoleApp"
+   language "C++"
+   includedirs { "../../" }
+   exceptionhandling "Off"
+   rtti "Off"
+
+   files { "main.cpp", "../../libs/stb_vorbis/stb_vorbis.c", "../../libs/minimp3/minimp3.cpp", "../../libs/tinf/tinflate.c" }
+
+   filter "system:Linux"
+      filter "toolset:gcc or clang"
+         links { "X11", "GL", "m", "pthread", "pulse-simple", "pulse" }
+	 defines { "POSIX_THREADS", "POSIX_READER_WRITER_LOCKS" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "Speed"
