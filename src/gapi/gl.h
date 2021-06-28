XProger/OpenLara

commit 5603cf2b0c9ec2da0ffa84c96a743afbb454b54f

fix build, simplify imports on macOS 11 and 10.15 (#355)
fix build on macOS 11 and 10.15, including on Apple silicon

There was a build error on macOS due to use of undeclared identifiers
glGetStringi and GL_NUM_EXTENSIONS. Those identifiers are declared in
OpenGL 3.0.

Switch from importing OpenGL/{gl.h,glext.h} to OpenGL/{gl3.h,gl3ext.h},
which fixes the problem. It also allows removing many OpenGL 3+ #defines,
though we do need to #define GL_LUMINANCE as was done in OpenGL/gl.h.
(It's also possible to keep the OpenGL/gl.h import, but it seems worse
to be importing both.)

Also remove #includes of Carbon/Carbon.h, OpenGL/OpenGL.h, AGL/agl.h,
since they don't seem to be needed.

Tested on macOS 11.4 (Big Sur) with Xcode 12.5.1 on Apple silicon,
and on macOS 10.15.7 (Catalina) with Xcode 12.4 on an Intel-based Mac.
(Hopefully this doesn't break older macOS platforms,
but unfortunately I can't test that easily.)

Fixes #354.

src/gapi/gl.h
@@ -271,32 +271,11 @@
         //#define GL_TEXTURE_COMPARE_FUNC     GL_TEXTURE_COMPARE_FUNC_EXT
         //#define GL_COMPARE_REF_TO_TEXTURE   GL_COMPARE_REF_TO_TEXTURE_EXT
     #else
-        #include <Carbon/Carbon.h>
         #include <AudioToolbox/AudioQueue.h>
-        #include <OpenGL/OpenGL.h>
-        #include <OpenGL/gl.h>
-        #include <OpenGL/glext.h>
-        #include <AGL/agl.h>
+        #include <OpenGL/gl3.h>
+        #include <OpenGL/gl3ext.h>
 
-        #define GL_RG                       0x8227
-        #define GL_RG16F                    0x822F
-        #define GL_RG32F                    0x8230
-        #define GL_RGBA16F                  0x881A
-        #define GL_RGBA32F                  0x8814
-        #define GL_HALF_FLOAT               0x140B
-
-        #define GL_RGB565                   GL_RGBA
-        #define GL_TEXTURE_COMPARE_MODE     0x884C
-        #define GL_TEXTURE_COMPARE_FUNC     0x884D
-        #define GL_COMPARE_REF_TO_TEXTURE   0x884E
-
-        #define glGenVertexArrays    glGenVertexArraysAPPLE
-        #define glDeleteVertexArrays glDeleteVertexArraysAPPLE
-        #define glBindVertexArray    glBindVertexArrayAPPLE
-
-        #define GL_PROGRAM_BINARY_LENGTH 0
-        #define glGetProgramBinary(...)  0
-        #define glProgramBinary(...)     0
+        #define GL_LUMINANCE 0x1909
     #endif
 #elif _OS_WEB
     #include <emscripten/emscripten.h>

commit 42ad7d6aab7cb5b03c4432454373d8ae27f81ce9

restore high graphics detail on macOS
The macOS platform currently creates and uses an OpenGL 2.1 profile,
but it can still achieve high graphics detail in theory. In practice,
that ability regressed.

Prior to PR #355, extSupport made use of an undeclared glGetStringi,
a build error. That was fixed by importing a header that declared it.
That caused extSupport to start reporting false for any input. This
is due to the (glGetStringi != NULL) check not having the intended
effect on macOS, as reported in #357.

As long as OpenGL 2.1 profile is used, extSupport can't make use
of the glGetStringi + GL_NUM_EXTENSIONS code path, so explicitly
disable it on macOS for now via a preprocessor guard.

Another regression introduced by #355 is the removal of redefines
of glGenVertexArrays, glDeleteVertexArrays, glBindVertexArray with
Apple-specific versions, needed to get VAO alongside OpenGL 2.1.
So, bring this back, along with the comment justifying why they're
needed (as was done before 6e6ed3b2d0e8c0c9af83d5cb9653793ca76047f4).
Import OpenGL/glext.h to get definitions of those APPLE functions.

With these changes, high graphics detail settings are available on
macOS once again.

Fixes #356.
Updates #357.

src/gapi/gl.h
@@ -274,6 +274,14 @@
         #include <AudioToolbox/AudioQueue.h>
         #include <OpenGL/gl3.h>
         #include <OpenGL/gl3ext.h>
+        #include <OpenGL/glext.h>
+
+        // In compatibility mode, macOS only provides OpenGL 2.1 (no VAO), but it does
+        // support the Apple-specific VAO extension which is older and in all relevant
+        // parts 100% compatible. So use those functions instead.
+        #define glGenVertexArrays    glGenVertexArraysAPPLE
+        #define glDeleteVertexArrays glDeleteVertexArraysAPPLE
+        #define glBindVertexArray    glBindVertexArrayAPPLE
 
         #define GL_LUMINANCE 0x1909
     #endif
@@ -1135,7 +1143,7 @@ namespace GAPI {
 
 
     bool extSupport(const char *str) {
-        #if !defined(_GAPI_GLES2) 
+        #if !defined(_GAPI_GLES2) && !_OS_MAC
         if (glGetStringi != NULL) {
             GLint count = 0;
             glGetIntegerv(GL_NUM_EXTENSIONS, &count); 