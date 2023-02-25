28a32ca17ea7ccc98097612d15dc9d7c660353d1
Aikku93 <aik@aol.com.au>
2023-02-25 20:41:12 +1100

    GBA: Improve ADPCM encoder
    Minimize RMSE of post-filtered output rather than pre-filtered

--- src/platform/gba/packer/ad4/AD4.h
+++ src/platform/gba/packer/ad4/AD4.h
@@ -4,7 +4,6 @@
 
 struct AD4State_t {
 	 int32_t zM1, zM2;
-	 int32_t Tap;
 	 int32_t Quant;
 	 int32_t Output;
 	uint32_t MaxOutputLevel;
@@ -13,7 +12,6 @@ struct AD4State_t {
 void AD4_Init(struct AD4State_t *State) {
 	State->zM1    = 0;
 	State->zM2    = 0;
-	State->Tap    = 0;
 	State->Quant  = 0x0800;
 	State->Output = 0;
 	State->MaxOutputLevel = 0;
@@ -28,24 +26,18 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 	uint8_t  n;
 	 int32_t zM1    = State->zM1;
 	 int32_t zM2    = State->zM2;
-	 int32_t Tap    = State->Tap;
 	 int32_t Quant  = State->Quant;
 	 int32_t Output = State->Output;
 	uint32_t MaxOutputLevel = State->MaxOutputLevel;
 	uint32_t FrameData = 0;
 	for(n=0;n<8;n++) {
-		//! Get input and apply pre-filter
-		//! Pre-filter: Hpre(z) = 1 - (7/8)z^-1
+		//! Get input, compute prediction, and quantize residue
+		//! Note that we minimize error of Output rather than Y, which implies
+		//! applying the post-filter in the analysis equation to get the residue.
 		int32_t X = Data[n];
-		{
-			int32_t d = Tap - (Tap >> 3);
-			X = X - d, Tap = X + d;
-		}
-
-		//! Compute prediction and quantize residue
 		int32_t P = zM1 - zM2;
-		int32_t R = X - P; {
-			R = (2*R + ((R < 0) ? (-Quant) : (+Quant))) / (2*Quant); //! (R + Sign[R]*(Quant/2)) / Quant
+		int32_t R = X - (P + (Output - (Output >> 3))); {
+			R /= Quant; //! <- Do NOT round off, or we get into a limit cycle
 			if(R < -8) R = -8;
 			if(R > +7) R = +7;
 		}
@@ -68,7 +60,6 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 	}
 	State->zM1    = zM1;
 	State->zM2    = zM2;
-	State->Tap    = Tap;
 	State->Quant  = Quant;
 	State->Output = Output;
 	State->MaxOutputLevel = MaxOutputLevel;
