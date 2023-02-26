0975391e461f96592ad474b54a21848cf4d1d8fa
Aikku93 <aik@aol.com.au>
2023-02-26 12:48:48 +1100

    GBA: Fix ADPCM issues with silence

--- src/platform/gba/packer/ad4/AD4.h
+++ src/platform/gba/packer/ad4/AD4.h
@@ -4,6 +4,7 @@
 
 struct AD4State_t {
 	 int32_t zM1, zM2;
+	 int32_t Tap;
 	 int32_t Quant;
 	 int32_t Output;
 	uint32_t MaxOutputLevel;
@@ -12,6 +13,7 @@ struct AD4State_t {
 void AD4_Init(struct AD4State_t *State) {
 	State->zM1    = 0;
 	State->zM2    = 0;
+	State->Tap    = 0;
 	State->Quant  = 0x0800;
 	State->Output = 0;
 	State->MaxOutputLevel = 0;
@@ -26,6 +28,7 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 	uint8_t  n;
 	 int32_t zM1    = State->zM1;
 	 int32_t zM2    = State->zM2;
+	 int32_t Tap    = State->Tap;
 	 int32_t Quant  = State->Quant;
 	 int32_t Output = State->Output;
 	uint32_t MaxOutputLevel = State->MaxOutputLevel;
@@ -36,8 +39,12 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 		//! applying the post-filter in the analysis equation to get the residue.
 		int32_t X = Data[n];
 		int32_t P = zM1 - zM2;
-		int32_t R = X - (P + (Output - (Output >> 3))); {
-			R /= Quant; //! <- Do NOT round off, or we get into a limit cycle
+		int32_t R = X - (P + (Tap - (Tap >> 3))); {
+#if 0 //! Lower RMSE, but sounds noisier
+			R = (2*R + ((R < 0) ? (-Quant) : (+Quant))) / (2*Quant); //! (R + Sign[R]*(Quant/2)) / Quant
+#else
+			R /= Quant;
+#endif
 			if(R < -8) R = -8;
 			if(R > +7) R = +7;
 		}
@@ -48,6 +55,12 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 		Output = Y + Output - (Output >> 3);
 		uint32_t Level = (uint32_t)((Output < 0) ? (-Output) : (+Output));
 		if(Level > MaxOutputLevel) MaxOutputLevel = Level;
+
+		//! Do the same, but for the encoding tap. This is needed to
+		//! avoid a limit oscillation on silence from round-off error.
+		//! Technically, it does mean a different output, but it should
+		//! be close enough to what we want that it shouldn't matter.
+		Tap = Y + Tap - ((Tap + 4 - (Tap < 0)) >> 3); //! Y + Round[Tap*7/8]
 
 		//! Update taps and push residue to frame
 		zM2 = zM1;
@@ -60,6 +73,7 @@ uint32_t AD4_EncodeFrame(struct AD4State_t *State, const int16_t *Data) {
 	}
 	State->zM1    = zM1;
 	State->zM2    = zM2;
+	State->Tap    = Tap;
 	State->Quant  = Quant;
 	State->Output = Output;
 	State->MaxOutputLevel = MaxOutputLevel;
