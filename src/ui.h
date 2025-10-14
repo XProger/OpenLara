bdc19308dd79ce48870bd7ac1222ebb4f911dde1
Dawva <dekan.ghazi@student.lut.fi>
2025-10-14 13:59:46 +0300

    fixed the help menu, it will now show the correct input for the action Show

--- src/ui.h
+++ src/ui.h
@@ -4,6 +4,7 @@
 #include "core.h"
 #include "mesh.h"
 #include "controller.h"
+#define UI_SHOW_FPS
 
 #define PICKUP_SHOW_TIME 5.0f
 #define SUBTITLES_SPEED  0.1f
@@ -541,6 +542,7 @@ namespace UI {
 
     void init(IGame *game) {
         ensureLanguage(Core::settings.audio.language);
+        updateHelpText();
         UI::game = game;
         showHelp = false;
         helpTipTime = 5.0f;
@@ -635,6 +637,7 @@ namespace UI {
         if (Input::down[ikH]) {
             Input::down[ikH] = false;
             showHelp = !showHelp;
+            updateHelpText();
             helpTipTime = 0.0f;
         }
         if (helpTipTime > 0.0f)
@@ -726,7 +729,7 @@ namespace UI {
 
     #if defined(_OS_WEB) || defined(_OS_WIN) || defined(_OS_LINUX) || defined(_OS_MAC) || defined(_OS_RPI)
         if (showHelp) {
-            textOut(vec2(32, 32), STR_HELP_TEXT, aLeft, width - 32, 255, UI::SHADE_GRAY);
+            textOut(vec2(32, 32), helpText, aLeft, width - 32, 255, UI::SHADE_GRAY);
         } else {
             if (helpTipTime > 0.0f) {
                 textOut(vec2(0, height - 16), STR_HELP_PRESS, aCenter, width, 255, UI::SHADE_ORANGE);
